cc930e7371fb6a74d4471a43bc7e0fcb859e23fa
Jane Thiem <janethiem7@gmail.com>
2025-09-20 09:23:16 -0700

    Configure db connections and add db connection str
    - updated Program.cs to register the db context and config sqllite
    - updated appsettings.json to include the sqllite connection string

--- src/WhiskerList.Api/Program.cs
+++ src/WhiskerList.Api/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WhiskerList.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();  // 1. include endpoint metadata
-builder.Services.AddSwaggerGen(o => // 2. generate OpenAPI/Swagger
+// Add Entity Framework and SQLite
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Swagger UI
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(o => 
 {
     o.SwaggerDoc("v1", new() { Title = "WhiskerList API", Version = "v1" });
-});            // 2. generate OpenAPI/Swagger
+});
 
 var app = builder.Build();
 
@@ -17,28 +25,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
